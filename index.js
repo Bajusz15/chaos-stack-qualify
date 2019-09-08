5eb4d3846a0c5bd9b64155d3c5bdd6ed3c7dd195
Patrikmulato <mulatopatrik@gmail.com>
2019-09-08 22:48:35 +0200

    utolso Patriknal

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 const request = require("request");
+const axios = require('axios')
+const mapping = require('./mapping.json')
 const keys = require("./keys");
 
-let options = {
+axios({
     method: 'GET',
     url: 'https://registration.hungary.chaosstack.com/api/qualify/user_commands',
     headers: { Authorization: keys.apiToken }
-};
+}).then(function (response) {
+    response.data.userCommands.map(command => {
 
-request(options, (error, response, body) => {
-    if (error) throw new Error(error);
+        mapping.map(person => {
+            if (command.user === person.user) {
+                //console.log(person.rooms[command.targetRoom]);
+                // console.log(`lights => ${person.rooms[command.targetRoom]} command => ${command.action}`)
+                let test = JSON.stringify({
+                    "desiredState": command.action,
+                    "lights": person.rooms[command.targetRoom]
+                })
 
-    //console.log(body);
-});
+                console.log(IsJsonString(test))
+                console.log(test)
+                console.log(command.user)
+                let postOptions = {
+                    method: 'POST',
+                    url: 'https://registration.hungary.chaosstack.com/api/qualify/set_lights',
+                    headers: { 'content-type': 'application/json', Authorization: keys.apiToken },
+                    json: {
+                        "desiredState": command.action,
+                        "lights": person.rooms[command.targetRoom]
+                    }
+                };
+                request(postOptions, (error, response, body) => {
+                    if (error) console.log(error);
 
+                    console.log(body);
+                });
+            }
+        })
+    })
+})
 
-
-let postOptions = {
-    method: 'POST',
-    url: 'https://registration.hungary.chaosstack.com/api/qualify/set_lights',
-    headers: { 'content-type': 'application/json', Authorization: keys.apiToken },
-     json: {
-         "desiredState": "on",
-         "lights": ["light-4f33566",
-             "light-4d4a9de",
-             "light-f7dcea4"]
-     }
-
-};
-
-request(postOptions, (error, response, body) => {
-    if (error) console.log(error);
-
-    console.log(body);
-});
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const keys = require("./keys");
+const mapping = require('./mapping.json')
+
 
 
 axios({
@@ -7,17 +9,22 @@ axios({
     url: 'https://registration.hungary.chaosstack.com/api/qualify/user_commands',
     headers: { Authorization: keys.apiToken }
 }).then(function (response) {
-    // console.log(response.data)
-});
-
-axios({
-    method: 'POST',
-    url: 'https://registration.hungary.chaosstack.com/api/qualify/set_lights',
-    headers: { 'Content-Type': 'application/json', Authorization: keys.apiToken },
-    body: {
-        'desiredState': "on",
-        'lights': ["light-4f33566",
-            "light-4d4a9de",
-            "light-f7dcea4"]
-    }
-}).catch(err => console.log(err));
+    console.log(response.data)
+    response.data.userCommands.map(command => {
+        mapping.map(person => {
+            if (command.user === person.user) {
+                // console.log(`lights => ${[person.rooms[command.targetRoom]]} command => ${command.action}`)
+                axios({
+                    method: 'POST',
+                    url: 'https://registration.hungary.chaosstack.com/api/qualify/set_lights',
+                    headers: { 'content-type': 'application/json', Authorization: keys.apiToken },
+                    json: {
+                        "desiredState": `${command.action}
+                        `,
+                        "lights": [person.rooms[command.targetRoom]]
+                    }
+                }).catch(err => console.log(err))
+            }
+        })
+    })
+})
